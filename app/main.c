54e803b9274c0458f74328866b200b5f1f103eed
VaclavHrbek <st43190@upce.cz>
2022-01-14 14:22:18 +0100

    Polish main function

--- app/main.c
+++ app/main.c
@@ -13,38 +13,28 @@ int main(void){
 
 	Population pop = { .size_pop = SIZE_OF_POPULAION, .num_of_nodes_ind = NUMBER_OF_NODES_IND};
 	create_population(&pop);
-	calculate_fitness(&pop);
-
 
 	for(size_t i = 0; i != NUM_OF_GENERATION; ++i){
+		printf("Generation: %ld \n", i);
 		calculate_fitness(&pop);
 
-		if(final_solution(pop.fitness, pop.size_pop) == true){
-			printf("Generation: %ld \n", i);
-			printf("Found best solution using genetic operation: \n");
-			print_best_individual(&pop);
-			return 0;
-		}else{
-			printf("Generation: %ld \n", i);
-			print_best_individual(&pop);
-		}
-
 		Population new_pop = optimize(pop);
-
 		if(final_solution(new_pop.fitness, new_pop.size_pop) == true){
-			printf("Generation: %ld \n", i);
 			printf("Found best solution using optimalization: \n");
 			print_best_individual(&new_pop);
 			return 0;
-		}else{
-			printf("Generation: %ld \n", i);
-			print_best_individual(&new_pop);
 		}
 
 		new_pop = genetic_operations(&pop, MUTAION_RATE, CROSSOUVER_RATE);
+		calculate_fitness(&new_pop);
+		if(final_solution(new_pop.fitness, new_pop.size_pop) == true){
+			printf("Found best solution using genetic operation: \n");
+			print_best_individual(&new_pop);
+			return 0;
+		}
+		print_best_individual(&pop);
 
 		pop = new_pop;
-		
 	}
 	return 0;
 }
